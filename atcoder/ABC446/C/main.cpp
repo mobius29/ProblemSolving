59b23412fb76794e59f5596c535733ca3c704709
mobius29 <sagjin0000@naver.com>
2026-02-21 23:23:06 +0900

    ABC446(AtCoder)

---
+++ atcoder/ABC446/C/main.cpp
@@ -0,0 +1,124 @@
+#include <array>
+#include <deque>
+#include <iostream>
+#include <tuple>
+#include <type_traits>
+#include <utility>
+#include <vector>
+
+#define ends ' '
+#define endl '\n'
+#define all(x) (x).begin(), (x).end()
+using namespace std;
+
+using ll = long long;
+using pii = pair<int, int>;
+template <typename T> using Vec = vector<T>;
+template <typename T> using Vec2 = Vec<Vec<T>>;
+
+template <typename T> T read();
+template <typename T> Vec<T> read(int size);
+template <typename T> Vec2<T> read(int r_size, int c_size);
+
+constexpr int INF = 0x3f3f3f3f;
+constexpr ll LINF = 0x3f3f3f3f3f3f3f3f;
+
+constexpr array<int, 4> dx = { { -1, 0, 1, 0 } };
+constexpr array<int, 4> dy = { { 0, -1, 0, 1 } };
+
+int solve(const int &N, const int &D, const Vec<int> &A, const Vec<int> &B) {
+  deque<pii> dq;
+  for (int i = 0; i < N; i++) {
+    dq.push_back({ A[i], i });
+
+    int needed = B[i];
+    while (needed > 0) {
+      auto [eggs, day] = dq.front();
+      dq.pop_front();
+
+      needed -= eggs;
+      if (needed < 0) dq.push_front({ -needed, day });
+    }
+
+    while (!dq.empty() && dq.front().second <= i - D) { dq.pop_front(); }
+  }
+
+  int ret = 0;
+  for (auto &[eggs, day] : dq) ret += eggs;
+
+  return ret;
+}
+
+int main() {
+  ios::sync_with_stdio(false);
+  cin.tie(nullptr);
+
+  // sum of N over all test cases is at most 200,000
+
+  auto T = read<int>(); // 1 <= T <= 200,000
+  while (T--) {
+    auto [N, D] = read<pii>(); // 1 <= D <= N <= 200,000
+    auto A = read<int>(N);
+    auto B = read<int>(N);
+
+    cout << solve(N, D, A, B) << endl;
+  }
+
+  return 0;
+}
+
+// --------------------- Helper Functions ---------------------
+
+template <typename T, class = void> struct is_tuple_like : false_type {};
+template <typename T> struct is_tuple_like<T, void_t<decltype(tuple_size<T>::value)>> : true_type {};
+template <typename T> inline constexpr bool is_tuple_like_v = is_tuple_like<T>::value;
+
+template <typename T, class = void> struct is_container : false_type {};
+template <typename T>
+struct is_container<T, void_t<decltype(begin(declval<T &>())), decltype(end(declval<T &>()))>> : true_type {};
+template <> struct is_container<string> : false_type {};
+template <typename T> inline constexpr bool is_container_v = is_container<T>::value;
+
+template <typename T, class = void> struct has_size : false_type {};
+template <typename T> struct has_size<T, void_t<decltype(declval<T &>().size())>> : true_type {};
+template <typename T> inline constexpr bool has_size_v = has_size<T>::value;
+
+template <typename T> T _read_primitive() {
+  T ret;
+  cin >> ret;
+  return ret;
+}
+
+template <typename T, size_t... Is> T _read_tuple_like(index_sequence<Is...>) {
+  T ret{};
+  ((get<Is>(ret) = read<decay_t<decltype(get<Is>(declval<T &>()))>>()), ...);
+  return ret;
+}
+
+template <typename T> T _read_container() {
+  T ret{};
+  for (auto &x : ret) x = read<decay_t<decltype(x)>>();
+  return ret;
+}
+
+template <typename T> T read() {
+  if constexpr (is_tuple_like_v<T>) {
+    return _read_tuple_like<T>(make_index_sequence<tuple_size<T>::value>{});
+  } else if constexpr (is_container_v<T> && has_size_v<T>) {
+    return _read_container<T>();
+  } else {
+    return _read_primitive<T>();
+  }
+}
+
+template <typename T> Vec<T> read(int size) {
+  Vec<T> v(size);
+  for (auto &el : v) el = read<T>();
+  return v;
+}
+
+template <typename T> Vec2<T> read(int r_size, int c_size) {
+  Vec2<T> v(r_size, Vec<T>(c_size));
+  for (auto &r : v) r = read<T>(c_size);
+  return v;
+}
